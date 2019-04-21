cc4993b5c6b18d0ce038024cebdfed3cdfc1b8c7
Serena Lertkantitham <serena.lertkantitham@gmail.com>
2019-04-21 15:13:19 -0700

    gunRoulette drill

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import HelloWorld from "./state-drills/HelloWorld";
-import Bomb from "./state-drills/Bomb";
+// import HelloWorld from "./state-drills/HelloWorld";
+// import Bomb from "./state-drills/Bomb";
+import RouletteGun from "./state-drills/RouletteGun";
 
 function App() {
   return (
     <main className="App">
-      <Bomb />
+      <RouletteGun />
     </main>
   );
 }
